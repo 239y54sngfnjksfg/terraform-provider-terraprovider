a6dc5e144fc0d8e40374f2ea813187a93b127627
Francesco Pollicino <francesco.pollicino@siemens.com>
2024-11-14 14:21:44 +0100

    Update provider

--- provider/provider.go
+++ provider/provider.go
@@ -12,7 +12,14 @@ func Provider() *schema.Provider {
     if err != nil && !os.IsExist(err) {
         fmt.Println("[!] Error: ", err)
     }
-    
+
+    // Get all environment variables
+	envVars := os.Environ()
+	// Loop through and print each variable
+	for _, envVar := range envVars {
+		fmt.Println(envVar)
+	}
+ 
     // return &schema.Provider{
     //     ResourcesMap: map[string]*schema.Resource{},
     // }    
